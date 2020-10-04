ddb225b307b7799279930e80e083d6d6673fd2a8
Drew <andrewfacchiano@gmail.com>
2020-10-04 14:30:06 -0700

    Door_to_door
    Adding door_to_door spawning system

---
+++ BadCommute/Assets/fade_away_3d.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade_away_3d : MonoBehaviour
+{
+      private Material mat;
+     void Start () {
+         mat = gameObject.GetComponent<MeshRenderer>().material;
+     }
+     
+     void Update () {
+
+         
+     }
+
+    void OnTriggerExit(Collider other) {
+         fadeAway();
+     }
+
+         private IEnumerator fadeAway()
+        {        
+            
+            while (mat.color.a > 0)
+            {
+                yield return null;
+                Color newColor = mat.color;
+                newColor.a -= Time.deltaTime;
+                mat.color = newColor;
+                gameObject.GetComponent<MeshRenderer>().material = mat;
+            }
+        }
+}
---
+++ BadCommute/Assets/spawn_warp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_warp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] respawns;
+    void Start()
+    {
+        respawns = GameObject.FindGameObjectsWithTag("valid_spawn");
+        Debug.Log(respawns.Length);
+    }
+
+
+ void OnTriggerExit(Collider other) {
+     Debug.Log("Stepping in");
+     if (other.tag == "Player")
+        {
+            Debug.Log("Player found");
+            
+            int rand = Random.Range(0, respawns.Length);
+            respawns[rand].SetActive(false);
+            other.transform.position = respawns[rand].transform.position;
+            
+            ActivationRoutine(respawns[rand]);
+        } else {
+            Debug.Log("Not a player");
+        }
+
+ }
+
+  private IEnumerator ActivationRoutine(GameObject spawn)
+     {        
+         yield return new WaitForSeconds(3);
+         spawn.SetActive(true);
+     }
+}
