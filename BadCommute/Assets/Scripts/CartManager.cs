25025e728592ae023a2e923ccaa2ba8edfaaefa5
Alec Asperslag <asperslaga@gmail.com>
2020-10-04 17:33:46 -0700

    SHow/Hide phantoms
    Cart manager

---
+++ BadCommute/Assets/Scripts/CartManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartManager : MonoBehaviour
+{
+    public GameObject[] PhantomList;
+    private Tick_tracker PromptManager;
+
+    private int lastTick;
+
+
+    public GameObject[] RealTicketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        PromptManager = GameObject.FindGameObjectWithTag("PromptManager").GetComponent<Tick_tracker>();
+        UpdateSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastTick != PromptManager.current_beat)
+        {
+            UpdateSpawn();
+        }
+    }
+
+    public void UpdateSpawn()
+    {
+        //Get data from Prompt manager
+        int currentStoryBeat = PromptManager.current_beat;
+        int maxBeats = PromptManager.story_beats.Length;
+
+
+        //Update Phantom Spawning
+        int spawnGroupSize = 0;
+        int totalToSpawn = 0;
+        if(currentStoryBeat > 1)
+        {
+            //Activate the appropriate Phantoms
+            spawnGroupSize = PhantomList.Length / (maxBeats - 1);
+            totalToSpawn = (spawnGroupSize * currentStoryBeat) - 1;
+        }
+        else if (currentStoryBeat == maxBeats)
+        {
+            totalToSpawn = PhantomList.Length;
+        }
+
+        //Show/Hide Phantoms
+        for(int i = 0; i < PhantomList.Length; i++)
+        {
+            if (i < totalToSpawn)
+            {
+                 PhantomList[i].SetActive(true);
+            }
+            else
+            {
+                PhantomList[i].SetActive(false);
+            }
+        }
+
+        //Update the Real Tickets
+        foreach(GameObject realTicket in RealTicketList)
+        {
+            realTicket.SetActive(currentStoryBeat == maxBeats);
+        }
+
+        //Track the last Story Beat to update
+        lastTick = currentStoryBeat;
+
+    }
+}
